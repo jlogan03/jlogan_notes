c707e0b0b32aa5135b44fe2cc0aac08b964f5b15
James Logan <jlogan03@gmail.com>
2025-05-18 15:45:44 -0400

    bode plot

--- conference_materials/SCR2025/filter_bode.py
+++ conference_materials/SCR2025/filter_bode.py
@@ -11,27 +11,34 @@ def rc_bode(r, c, w=None):
     den = [r * c, 1]
     system = signal.TransferFunction(num, den)
 
-    # Frequency range for Bode plot
-    w = w if w is not None else np.logspace(1, 6, 500)  # from 10 to 1,000,000 rad/s
-
     # Frequency response
+    w = w if w is not None else np.logspace(1, 6, 500)  # [rad/s]
     w, mag, phase = signal.bode(system, w)
 
     return system, w, mag, phase
 
 
-def butter_bode(cutoff_ratio, order, w_norm=None):
-    """Normalized digital butterworth filter"""
+def butter_bode(cutoff_ratio, order, fs, w=None):
+    """Digital Butterworth with some cutoff as a fraction of samplerate"""
 
     # Transfer function
-    system = signal.butter(order, Wn=cutoff_ratio * 2.0 * np.pi, analog=False, fs=1.0)
+    system = signal.butter(order, Wn=cutoff_ratio * fs, analog=False, fs=fs)
 
-    # Frequency range for Bode plot
-    w_norm = w_norm if w_norm is not None else np.logspace(-3, 0, 500)
+    # Frequency response
+    w = w if w is not None else np.logspace(1, 6, 500)  # [rad/s]
+    w, mag, phase = signal.dbode(signal.dlti(*system, dt=1.0 / fs), w=w / fs)
 
-    w_norm, mag, phase = signal.dbode(signal.dlti(*system, dt=1.0), w_norm)
+    return system, w, mag, phase
 
-    return system, w_norm, mag, phase
+
+def inv_db(x):
+    """Inverse decibel scaling"""
+    return 10.0 ** (x / 20.0)
+
+
+def db(x):
+    """Decibel scaling"""
+    return 20.0 * np.log10(x)
 
 
 # RC analog filter params
@@ -43,48 +50,116 @@ internal_samplerate = 40e3  # [Hz]
 reporting_rate = 1e3  # [Hz]
 rc_cutoff = 1 / (2.0 * np.pi * r * c)  # [Hz]
 #   [dimensionless] butterworth filter cutoff as a fraction of samplerate
+#   with cutoff parked at reporting rate, allowing some aliasing in exchange for
+#   reduced phase distortion
 butter_cutoff_ratio = reporting_rate / internal_samplerate
 
 # RC filter freq response
 rc_sys, rc_w, rc_mag, rc_phase = rc_bode(r, c)
 
 # Butterworth filter freq response
-reporting_rate_rad_per_s = reporting_rate * 2.0 * np.pi
-w = rc_w[np.where(rc_w < reporting_rate_rad_per_s)]
-w_norm = w / reporting_rate_rad_per_s
+# Examined only below the internal samplerate
+inds = np.where(rc_w < internal_samplerate * 2.0 * np.pi)
+w = rc_w[inds]
 butter_sys, butter_w, butter_mag, butter_phase = butter_bode(
-    butter_cutoff_ratio, order=2, w_norm=w_norm
+    butter_cutoff_ratio, order=2, fs=internal_samplerate, w=w
 )
-butter_w = butter_w * reporting_rate * 2.0 * np.pi  # [rad/s] from normalized form
+
+# Total freq response
+total_mag = db(inv_db(rc_mag[inds]) * inv_db(butter_mag))  # [dB]
+total_phase = rc_phase[inds] + butter_phase  # [deg]
 
 # Magnitude
 plt.figure(figsize=(12, 8))
 plt.subplot(2, 1, 1)
-plt.semilogx(rc_w / (2.0 * np.pi), rc_mag, color="k", linestyle="dotted")
-plt.semilogx(butter_w / (2.0 * np.pi), butter_mag, color="k", linestyle="--")
-plt.title("Bode Plot of First-order RC Low-pass Filter")
+plt.semilogx(
+    rc_w / (2.0 * np.pi),
+    rc_mag,
+    color="k",
+    linestyle="dotted",
+    label="ADC Input Filter",
+)
+plt.semilogx(
+    butter_w / (2.0 * np.pi),
+    butter_mag,
+    color="k",
+    linestyle="--",
+    label="Digital Filter",
+)
+plt.semilogx(butter_w / (2.0 * np.pi), total_mag, color="k", linestyle="-", label="Aggregate Filter")
+plt.title("Bode Plot of Filter Stages 2 & 3")
 plt.ylabel("Magnitude [dB]")
 plt.grid(which="both", linewidth=0.5)
 
-plt.axvline(x=40e3, color="k", linestyle="--", label="Internal Samplerate")
-plt.axvline(x=1e3, color="k", linestyle="-", label="Typical Reporting Rate")
-plt.axvline(
-    x=1 / (2.0 * np.pi * r * c), color="k", linestyle="dotted", label="RC Cutoff"
+ymin, ymax = plt.ylim()
+dy = ymax - ymin
+
+plt.axvline(x=internal_samplerate, color="k", linestyle="--")
+plt.annotate(
+    "Internal Samplerate",
+    xy=(internal_samplerate, ymax - dy / 4),
+    xytext=(20, -10),
+    textcoords='offset points',
+    arrowprops={"linewidth": 1, "facecolor": 'k', "arrowstyle": '->,head_width=.25'},
+    ha="left",
 )
+
+plt.axvline(x=internal_samplerate / 2, color="k", linestyle="--")
+plt.annotate(
+    "Nyquist of\nInternal Samplerate",
+    xy=(internal_samplerate / 2, ymax - dy / 8),
+    xytext=(-20, -10),
+    textcoords='offset points',
+    arrowprops={"linewidth": 1, "facecolor": 'k', "arrowstyle": '->,head_width=.25'},
+    ha="right",
+)
+
+plt.axvline(x=reporting_rate, color="k", linestyle="--")
+plt.annotate(
+    "Typical Reporting Rate",
+    xy=(reporting_rate, ymax - 2 * dy / 4 ),
+    xytext=(20, -5),
+    textcoords='offset points',
+    arrowprops={"linewidth": 1, "facecolor": 'k', "arrowstyle": '->,head_width=.25'},
+    ha="left",
+)
+
+plt.axvline(x=reporting_rate / 2, color="k", linestyle="-", linewidth=3)
+plt.annotate(
+    "Nyquist of\nTypical Reporting Rate",
+    xy=(reporting_rate / 2, ymax - 3 * dy / 4 ),
+    xytext=(-20, -10),
+    textcoords='offset points',
+    arrowprops={"linewidth": 1, "facecolor": 'k', "arrowstyle": '->,head_width=.25'},
+    ha="right",
+)
+
+plt.axvline(x=rc_cutoff, color="k", linestyle="--")
+plt.annotate(
+    "ADC Input\nFilter Cutoff",
+    xy=(rc_cutoff, ymax - 3 * dy / 4 ),
+    xytext=(-20, -10),
+    textcoords='offset points',
+    arrowprops={"linewidth": 1, "facecolor": 'k', "arrowstyle": '->,head_width=.25'},
+    ha="right",
+)
+
+plt.legend()
 
 # Phase
 plt.subplot(2, 1, 2)
-plt.semilogx(rc_w / (2.0 * np.pi), rc_phase, color="k", linestyle="dotted")
-plt.semilogx(butter_w / (2.0 * np.pi), butter_phase, color="k", linestyle="--")
+plt.semilogx(rc_w / (2.0 * np.pi), rc_phase, color="k", linestyle="dotted", label="ADC Input Filter")
+plt.semilogx(butter_w / (2.0 * np.pi), butter_phase, color="k", linestyle="--", label="Digital Filter")
+plt.semilogx(butter_w / (2.0 * np.pi), total_phase, color="k", linestyle="-", label="Aggregate Filter")
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("Phase [deg]")
 plt.grid(which="both", linewidth=0.5)
 
-plt.axvline(x=40e3, color="k", linestyle="--", label="Internal Samplerate")
-plt.axvline(x=1e3, color="k", linestyle="-", label="Typical Reporting Rate")
-plt.axvline(
-    x=1 / (2.0 * np.pi * r * c), color="k", linestyle="dotted", label="RC Cutoff"
-)
+plt.axvline(x=internal_samplerate, color="k", linestyle="--")
+plt.axvline(x=internal_samplerate / 2, color="k", linestyle="--")
+plt.axvline(x=reporting_rate, color="k", linestyle="--")
+plt.axvline(x=reporting_rate / 2, color="k", linestyle="-", linewidth=3)
+plt.axvline(x=rc_cutoff, color="k", linestyle="--")
 
 plt.legend()
 
