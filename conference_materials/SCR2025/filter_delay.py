64586eb9b5555d9449149fd524fb32af219aea3e
James Logan <jlogan03@gmail.com>
2025-05-25 15:37:51 -0400

    add plot of time domain response

--- conference_materials/SCR2025/filter_delay.py
+++ conference_materials/SCR2025/filter_delay.py
@@ -59,6 +59,9 @@ for order in [3, 4, 6]:
 
     taps = [poly_taps(order, delay) for delay in fractional_delays]
 
+    #
+    # Frequency response
+    #
     w = 2.0 * np.pi * samplerate * np.logspace(-4, np.log10(0.5), 1000)
     responses = []
     for b in taps:
@@ -67,7 +70,7 @@ for order in [3, 4, 6]:
         phase = np.unwrap(np.angle(h, deg=True))  # [deg] phase lag frequency response
         responses.append((mag, phase))
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8), sharex=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(6, 8))
     plt.suptitle(f"Per-Channel Fractional Delays\nPoly Order = {order - 1} ({order}-Tap), Samplerate = {samplerate * 1e-3:.2f} kHz")
     for mag, phase in responses:
         plt.sca(ax1)
@@ -78,6 +81,7 @@ for order in [3, 4, 6]:
             alpha=0.4,
             linestyle="-",
         )
+        plt.xlabel("Frequency [Hz]")
         plt.ylabel("Magnitude [dB]")
 
         plt.sca(ax2)
@@ -99,7 +103,38 @@ for order in [3, 4, 6]:
     plt.axvline(samplerate / 2, color='k', linewidth=1,)
     plt.axvline(reporting_rate, color='k', linewidth=3,)
 
-    plt.tight_layout()
+    #
+    # Example time-domain response
+    #
+    plt.sca(ax3)
+    func = lambda x: np.sin(2.0 * np.pi * x * samplerate / 8)
+
+    #   Ideal signal
+    t = np.linspace(0.0, 8.0 * sample_period, 1000)
+    y = func(t)
+    plt.plot(t / 1e6, func(t), color='k', label="Signal")
+
+    #   Target sample times
+    sample_t = np.arange(0.0, 9.0 * sample_period, sample_period)
+
+    #   Actual samples from each group
+    for i in range(len(groups)):
+        delay = -delay_per_group * i
+        group_sample_time = sample_t - delay
+        group_sample_values = func(group_sample_time)
+        label = "Group Samples" if i ==0 else None
+        plt.scatter(group_sample_time / 1e6, group_sample_values, color='k', alpha=1.0, marker="|", s=100, label=label)
+
+        corrected_samples = np.convolve(taps[i], group_sample_values, mode="valid")
+        label = "Corrected Group Samples" if i ==0 else None
+        plt.scatter([sample_t[order-1:] / 1e6], corrected_samples, color='r', alpha=1.0, marker="_", s=100, label=label)
+
+    #   Corrected sample values applied to target sample time
+    plt.scatter(sample_t / 1e6, func(sample_t), color='r', marker="|", s=500, label="Target Sample Time")
+
+    plt.xlabel("Time [us]")
+
+    plt.legend(loc='upper left', bbox_to_anchor=(0.0, 1.0))
 
     plt.savefig(f"./fractional_delay_order{order}.svg")
 
