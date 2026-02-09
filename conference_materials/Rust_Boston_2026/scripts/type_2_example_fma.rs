914c629b86ce1a069c64b6b2f17f1826bf117a2d
James Logan <jlogan03@gmail.com>
2026-02-08 22:53:12 -0500

    parallel variant with FMA & as-presented slides pdf

--- conference_materials/Rust_Boston_2026/scripts/type_2_example_fma.rs
+++ conference_materials/Rust_Boston_2026/scripts/type_2_example_fma.rs
@@ -79,3 +79,32 @@ pub fn solid_angle_tetrahedron(
 
     Ok(())
 }
+
+use rayon::prelude::*;
+use std::sync::LazyLock;
+
+/// Populated once on first access, then never again.
+/// No lock used for access after initialization!
+static PHYSICAL_CORES: LazyLock<usize> = LazyLock::new(num_cpus::get_physical);
+
+/// Vector-parallel variant of [solid_angle_tetrahedron_scalar]
+#[inline] // Enable cross-crate inlining
+pub fn solid_angle_tetrahedra_par(
+    tetrahedra: &[[[f64; 3]; 4]],
+    out: &mut [f64],
+) -> Result<(), &'static str> {
+    // Chunk inputs. Only use real cores!
+    let num_cores = rayon::current_num_threads().min(*PHYSICAL_CORES);
+    let num_chunks = 1024.min(out.len() / num_cores);
+    let (tet_chunks, out_chunks) = (
+        tetrahedra.par_chunks(num_chunks),
+        out.par_chunks_mut(num_chunks),
+    );
+
+    // Do vector calculations over each chunk in parallel
+    (tet_chunks, out_chunks)
+        .into_par_iter()
+        .try_for_each(|(tetc, outc)| solid_angle_tetrahedron(tetc, outc))?;
+
+    Ok(())
+}
